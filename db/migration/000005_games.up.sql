59f132c684d4f17a19a563dc09bacebde6be0197
salman69e27 <asbayomy04@gmail.com>
2021-10-05 03:33:18 +0200

    Add game table to database

---
+++ db/migration/000005_games.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE games (
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	white TEXT NOT NULL,
+	black TEXT NOT NULL,
+	result ENUM("1-0", "0.5-0.5", "0-1"),
+	white_match_side TEXT NOT NULL,
+	black_match_side TEXT NOT NULL,
+	match_id INTEGER NOT NULL,
+	CONSTRAINT fk_game_match_id
+	FOREIGN KEY (match_id) REFERENCES matches(id),
+	round_id INTEGER NOT NULL,
+	CONSTRAINT fk_game_round_id
+	FOREIGN KEY (round_id) REFERENCES rounds(id)
+);
+
+CREATE INDEX idx_game_match ON games(match_id);
+CREATE INDEX idx_game_round ON games(round_id);
